21882094bc6402ba3d7cd2331bd08039e041bce7
faith <89432125+faithchongrui@users.noreply.github.com>
2023-11-22 10:08:43 +0800

    fix

---
+++ Stupidathon2/Stupidathon2App.swift
@@ -0,0 +1,17 @@
+//
+//  Stupidathon2App.swift
+//  Stupidathon2
+//
+//  Created by FAITH CHONG RUI EN stu on 22/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Stupidathon2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
