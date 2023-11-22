54968601d4bf6efae2b27055a715356997364e61
faith <89432125+faithchongrui@users.noreply.github.com>
2023-11-22 09:57:14 +0800

    Initial Commit

---
+++ Stupidathon v2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stupidathon v2
+//
+//  Created by FAITH CHONG RUI EN stu on 22/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
